b794ec2b6a351e81b34dbba793b49645aa4504af
Geoff Lee <g.lee@ed.ac.uk>
2017-11-28 20:13:54 +0000

    Adapter class so this can be used in micropython

---
+++ library/bme680/i2c.py
@@ -0,0 +1,32 @@
+import sys
+
+try:
+    from machine import I2C
+except ImportError:
+    raise ImportError("Can't find the micropython machine.I2C class: "
+                      "perhaps you don't need this adapter?")
+
+
+class I2CAdapter(I2C):
+    """ Adds some of the SMBus I2c methods to the micropython I2c class,
+        for enhanced compatibility """
+
+    def read_byte_data(self, addr, register):
+        """ Read a single byte from register of device at addr
+            Returns a single byte """
+        return self.readfrom_mem(addr, register, 1)[0]
+
+    def read_i2c_block_data(self, addr, register, length):
+        """ Read a block of length from register of device at addr
+            Returns a bytes object filled with whatever was read """
+        return self.readfrom_mem(addr, register, length)
+
+    def write_byte_data(self, addr, register, data):
+        """ Write a single byte of data to register of device at addr
+            Returns None """
+        return self.writeto_mem(addr, register, data)
+
+    def write_i2c_block_data(self, addr, register, data):
+        """ Write multiple bytes of data to register of device at addr
+            Returns None """
+        return self.writeto_mem(addr, register, data)
